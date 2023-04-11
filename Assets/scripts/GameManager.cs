e4d3be0daa1473db5ac07362787e3a87fe05ec09
omar mansy <omarmansy96@gmail.com>
2023-04-11 17:29:50 +0200

    first commit

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ball;
+    public Transform ballPosition;
+    Plane plane = new Plane(Vector3.forward,0);
+    public Transform target;
+    public float ballSpeed;
+    public GameObject newBall;
+    bool ballReady;
+    void Start()
+    {
+        InitBall();
+    }
+
+    void Update()
+    {
+
+        Vector3 dir = target.position - ball.transform.position;
+        if (Input.GetMouseButtonDown(0)&& ballReady)
+        {
+            newBall.GetComponent<Rigidbody>().AddForce(dir * ballSpeed, ForceMode.Impulse);
+
+            ballReady = false;
+            Invoke(nameof(InitBall), 1);
+        }
+
+        float distance;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out distance))
+        {
+            Vector3 point = ray.GetPoint(distance);
+            target.position = new Vector3(point.x, point.y,7.5f);
+        }
+    }
+    void InitBall()
+    {
+        newBall = Instantiate(ball, ballPosition);
+        ballReady = true;
+    }
+}
